f0073de05a74c6a27154e125eede84d296730dc8
Liu7i <liu67iq@gmail.com>
2025-01-16 10:47:27 +0800

    启用懒加载优化首屏加载速度

--- App.tsx
+++ App.tsx
@@ -8,16 +8,17 @@
 import React, {Suspense, lazy} from 'react';
 import {StyleProp, View, ViewStyle} from 'react-native';
 import Home from './src/pages/Home';
-import Hisui from './src/pages/Hisui';
-import Paldea from './src/pages/Paldea';
-import Kitakami from './src/pages/Kitakami';
-import Blueberry from './src/pages/Blueberry';
+const Hisui = lazy(() => import('./src/pages/Hisui'));
+const Paldea = lazy(() => import('./src/pages/Paldea'));
+const Kitakami = lazy(() => import('./src/pages/Kitakami'));
+const Blueberry = lazy(() => import('./src/pages/Blueberry'));
 import {globalColor} from './src/globalStyle';
 import {observer} from '@quarkunlimit/qu-mobx';
 import {Provider, useStore} from './src/globalStore/index';
 import {LoadingModal} from './src/layout/LoadingModal';
 import {EPage} from './src/interface';
 import {ToolBar} from './src/components/ToolBar';
+import {QText} from './src/components/QText';
 
 const backgroundStyle: StyleProp<ViewStyle> = {
   backgroundColor: globalColor.background,
@@ -32,11 +33,18 @@ const App = observer(function App_(): React.JSX.Element {
     <View style={backgroundStyle}>
       <LoadingModal />
       <View style={{flex: 1}}>
-        {logic.currentPage === EPage.Home && <Home />}
-        {logic.currentPage === EPage.Hisui && <Hisui />}
-        {logic.currentPage === EPage.Paldea && <Paldea />}
-        {logic.currentPage === EPage.Kitakami && <Kitakami />}
-        {logic.currentPage === EPage.Blueberry && <Blueberry />}
+        <Suspense
+          fallback={
+            <View>
+              <QText>页面加载中...</QText>
+            </View>
+          }>
+          {logic.currentPage === EPage.Home && <Home />}
+          {logic.currentPage === EPage.Hisui && <Hisui />}
+          {logic.currentPage === EPage.Paldea && <Paldea />}
+          {logic.currentPage === EPage.Kitakami && <Kitakami />}
+          {logic.currentPage === EPage.Blueberry && <Blueberry />}
+        </Suspense>
       </View>
       <ToolBar />
     </View>
